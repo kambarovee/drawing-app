76c4fb710a514956f80230a3d559041afe4a0dfe
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-01-04 16:09:58 +0600

    Adding "onDraw" function

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -33,6 +33,18 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPaint!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {

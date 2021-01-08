64f34f1d01529b37305c33cd65f4560273acd66b
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-01-08 15:33:00 +0600

    Added function to set the selected color to DrawingView

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawing_view.setSizeForBrush(20.toFloat())
+
+        mImageButtonCurrentPaint = ll_paint_colors[4] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_selected))
+
         ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
     }
 
